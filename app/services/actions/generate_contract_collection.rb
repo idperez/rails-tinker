2e73aeb5099210e2e7d1ed5c5c1c6d80ba69827e
Isidro Perez <isidro.perez@sendoso.com>
2023-05-21 18:42:53 -0700

    lint

--- app/services/actions/generate_contract_collection.rb
+++ app/services/actions/generate_contract_collection.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Actions
   class GenerateContractCollection
     extend ::LightService::Action
@@ -9,36 +11,32 @@ module Actions
       contract_collection = Contracts::Csv::Collection.new
 
       context.csv.by_row.each.with_index do |row, row_number|
-        error_message = ""
+        error_message = ''
         missing_fields = []
         input_errors = []
 
-        missing_fields.push("Supplier") if row["Supplier"].blank?
-        missing_fields.push("Contract Value") if row["Contract Value"].blank?
-        missing_fields.push("Start Date") if row["Start Date"].blank?
-        missing_fields.push("End Date") if row["End Date"].blank?
-        missing_fields.push("Contract Name") if row["Contract Name"].blank?
-        missing_fields.push("External Contract ID") if row["External Contract ID"].blank?
+        missing_fields.push('Supplier') if row['Supplier'].blank?
+        missing_fields.push('Contract Value') if row['Contract Value'].blank?
+        missing_fields.push('Start Date') if row['Start Date'].blank?
+        missing_fields.push('End Date') if row['End Date'].blank?
+        missing_fields.push('Contract Name') if row['Contract Name'].blank?
+        missing_fields.push('External Contract ID') if row['External Contract ID'].blank?
 
-        if Monetize.parse(row["Contract Value"]).cents.abs.zero?
-          input_errors.push("Contract Value must be greater than 0")
+        if Monetize.parse(row['Contract Value']).cents.abs.zero?
+          input_errors.push('Contract Value must be greater than 0')
         end
-        if (DateTime.strptime(row["Start Date"], '%m/%d/%Y') rescue nil).blank?
-          input_errors.push("Start Date must be a date format")
+        if (DateTime.strptime(row['Start Date'], '%m/%d/%Y') rescue nil).blank?
+          input_errors.push('Start Date must be a date format')
         end
-        if (DateTime.strptime(row["End Date"], '%m/%d/%Y') rescue nil).blank?
-          input_errors.push("End Date must be a date format")
+        if (DateTime.strptime(row['End Date'], '%m/%d/%Y') rescue nil).blank?
+          input_errors.push('End Date must be a date format')
         end
-        if input_errors.blank? && DateTime.strptime(row["Start Date"], '%m/%d/%Y') >= DateTime.strptime(row["End Date"], '%m/%d/%Y')
-          input_errors.push("Start Date must be before End Date")
+        if input_errors.blank? && DateTime.strptime(row['Start Date'], '%m/%d/%Y') >= DateTime.strptime(row['End Date'], '%m/%d/%Y')
+          input_errors.push('Start Date must be before End Date')
         end
 
-        if missing_fields.present?
-          error_message.concat("Missing Fields: #{missing_fields.join(", ")}\n")
-        end
-        if input_errors.present?
-          error_message.concat("Input Errors: #{input_errors.join(", ")}\n")
-        end
+        error_message.concat("Missing Fields: #{missing_fields.join(', ')} ") if missing_fields.present?
+        error_message.concat("Input Errors: #{input_errors.join(', ')} ") if input_errors.present?
 
         if error_message.present?
           contract_collection.add_invalid_contract(row_number + 1, error_message)
